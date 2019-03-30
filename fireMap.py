b196fbd5d1654eaa40b02c9fe28c7b10d63881f6
Lukas Zierahn <lukas@kappa-mm.de>
2019-03-30 01:01:49 +0000

    its not working, I do give up

--- fireMap.py
+++ fireMap.py
@@ -1,6 +1,9 @@
 import time
 import main
-import mil.afrl.amase.utils.CmasiNavUtils
+from Helper import *
+from math import *
+from cmath import *
+from afrl.cmasi.Location3D import Location3D
 
 class FireMap():
 
@@ -8,6 +11,8 @@ class FireMap():
         self.main = main
         self.width = int(lmcpObject.Boundary.Width)
         self.height = int(lmcpObject.Boundary.Height)
+        self.middleWidth = self.width / 2.0
+        self.middleHeight = self.height / 2.0
         self.center = lmcpObject.Boundary.CenterPoint
 
         print("Creating Map with dimensions %d/%d" % (self.width, self.height))
@@ -18,13 +23,47 @@ class FireMap():
         print("Map created in %.3f seconds" % (time.time() - millis))
 
     def HandleAirVehicleState(self, msg):
-        print(msg.Location)
+        pass
+        #print(msg.Location)
 
     def LocationToCoord(self, location):
-        pass
+        d = distance((self.center.Latitude, self.center.Longitude), (location.Latitude, location.Longitude))
+        angle = calculate_initial_compass_bearing((self.center.Latitude, self.center.Longitude), (location.Latitude, location.Longitude))
+
+        offsetx = cos(angle) * d
+        offsety = sin(angle) * d
+
+        return self.middleWidth + offsetx, self.middleHeight + offsety
 
     def CoordToLocation(self, x, y):
-        pass
+        #source https://stackoverflow.com/questions/7222382/get-lat-long-given-current-point-distance-and-bearing/7835325
+        #this is heavily modified though
+
+        middle = complex(self.middleWidth, self.middleHeight)
+        pos = complex(x, y)
+        print(middle, pos)
+        d, angle = polar(middle - pos)
+
+        angle = (angle + pi/2.0) % 2*pi
+
+        print(d, degrees(angle), angle)
+
+        d = d / 1000.0
+        R = 6378.1 #Radius of the Earth
+
+        lat1 = math.radians(self.center.Latitude)
+        lon1 = math.radians(self.center.Longitude)
+
+        result = Location3D()
+
+        result.Latitude = math.asin(math.sin(lat1)*math.cos(d/R) +
+             math.cos(lat1)*math.sin(d/R)*math.cos(angle))
+
+        result.Longitude = lon1 + math.atan2(math.sin(angle)*math.sin(d/R)*math.cos(lat1),
+             math.cos(d/R)-math.sin(lat1)*math.sin(result.Latitude))
+
+        return result
+
 
     def getPoint(self, x, y):
         return self.data[y * self.width + y]
--- uav.py
+++ uav.py
@@ -1,5 +1,6 @@
 
 from main import *
+from afrl.cmasi.searchai.HazardSensorConfiguration import HazardSensorConfiguration
 
 
 class UAV():
@@ -9,7 +10,12 @@ class UAV():
         self.ID = msg.ID
         self.x = 0
         self.y = 0
+        self.sensorRange = -1
         self.status = None
+
+        for payload in msg.PayloadConfigurationList:
+            if isinstance(payload, HazardSensorConfiguration):
+                self.sensorRange = payload.MaxRange
 
         if (msg.EntityType.decode("utf-8") == "FixedWing"):
             self.fixedWing = True
@@ -18,8 +24,10 @@ class UAV():
         else:
             print("Undefined EntityType: " + msg.EntityType.decode("utf-8"))
 
-        print("Created UAV with:\n\tID: %d\n\tFixedWing: %s\n" % (self.ID, self.fixedWing))
+        print("Created UAV with:\n\tID: %d\n\tSensorRange: %d\n\tFixedWing: %s\n" % (self.ID, self.sensorRange, self.fixedWing))
 
     def UpdateUAV(self, msg):
         self.x, self.y = self.main.fireMap.LocationToCoord(msg.Location)
+        percieved = self.main.fireMap.CoordToLocation(self.x, self.y)
+        print((percieved.Latitude, percieved.Longitude), (msg.Location.Latitude, msg.Location.Longitude))
         self.status = msg
